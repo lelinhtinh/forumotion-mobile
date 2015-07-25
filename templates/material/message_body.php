cf4d0b6b4068e9b418ef06633f5635058347f193
Zzbaivong <baivong@users.noreply.github.com>
2015-07-26 06:08:49 +0700

    default

--- templates/material/message_body.php
+++
@@ -1,5 +0,0 @@
-<!-- BEGIN spacer --><br /><!-- END spacer -->
-<div class="message_die">
-	<div class="message_title" style="">{MESSAGE_TITLE}</div>
-	<div class="message_text" style="">{MESSAGE_TEXT}</div>
-</div>
