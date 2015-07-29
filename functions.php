7adaf5122890d66fb1436530009a0f9d74fed719
Zzbaivong <baivong@users.noreply.github.com>
2015-07-29 16:03:25 +0700

    Move topics_list function to viewforum

--- functions.php
+++ functions.php
@@ -19,7 +19,7 @@ function category($forumrow, $forumsArr) {
 
 		$v = $forumsObj->$k;
 
-		if($robot) {
+		if ($robot) {
 			$item = str_replace('{catrow.forumrow.U_VIEWFORUM}', $k . '-forum.html', $forumrow);
 		} else {
 			$item = str_replace('{catrow.forumrow.U_VIEWFORUM}', 'viewforum.php?selected_id=' . $k, $forumrow);
@@ -49,97 +49,6 @@ function information($text) {
 	$information = str_replace('{MESSAGE_TEXT}', $text, $information);
 
 	echo $information;
-}
-
-$topics_list = file_get_contents('../templates/' . $path . '/topics_list_box.php');
-
-function tempTopicsList($code, $html) {
-	global $topics_list;
-
-	return str_replace('{' . $code . '}', $html, $topics_list);
-}
-
-function topics_list() {
-	global $selected_id, $topics_list;
-
-	$topics_list = preg_replace('/<\!\-\-\sBEGIN\smulti_selection\s\-\->([\s\S]+?)<\!\-\-\sEND\smulti_selection\s\-\->/', '<!-- Multi selection -->', $topics_list);
-
-	$topics_list = preg_replace('/<\!\-\-\sBEGIN\sheader_row\s\-\->([\s\S]+)<\!\-\-\sEND\sheader_row\s\-\->/', '<!-- Header row -->', $topics_list);
-	$topics_list = str_replace('<!-- BEGIN spacer --><br /><!-- END spacer -->', '<!-- Spacer -->', $topics_list);
-
-	if (preg_match('/^f(6|11)$/', $selected_id)) {
-
-		$topics_list = preg_replace('/<\!\-\-\sBEGIN\stopic\s\-\->([\s\S]+)<\!\-\-\sEND\stopic\s\-\->/', '<!-- Topic -->', $topics_list);
-		$topics_list = tempTopicsList('topics_list_box.row.L_TITLE', 'Topics');
-		$topics_list = tempTopicsList('topics_list_box.row.L_NO_TOPICS', 'No message.');
-
-	} else {
-
-		$topics_list = preg_replace('/<\!\-\-\sBEGIN\sno_topics\s\-\->([\s\S]+)<\!\-\-\sEND\sno_topics\s\-\->/', '<!-- No topic -->', $topics_list);
-
-		$topics_list = preg_replace_callback('/<\!\-\-\sBEGIN\srow\s\-\->([\s\S]+)<\!\-\-\sEND\srow\s\-\->/', function ($arg) {
-			global $robot;
-			$topicsObj = json_decode(file_get_contents('../translate/data/topics.json'));
-
-			$topicrow = $arg[1];
-			$txt = '';
-
-			foreach ($topicsObj as $key => $value) {
-				$row = $topicrow;
-				$status = $value->{'status'};
-				$topic_type = '';
-				if(preg_match('/_global/', $status)) {
-					$topic_type = '<strong>Global announcement:</strong> ';
-				} else if(preg_match('/_announce/', $status)) {
-					$topic_type = '<strong>Announcement:</strong> ';
-				} else if(preg_match('/_sticky/', $status)) {
-					$topic_type = '<strong>Sticky:</strong> ';
-				}
-				if (!empty($value->{'poll'})) {
-					$topic_type .= '<strong>[ Poll ]</strong> ';
-				}
-
-
-				$row = str_replace('{topics_list_box.row.L_TITLE}', 'Announcement & Sticky', $row);
-				$row = str_replace('{topics_list_box.row.topic.table_sticky.L_TITLE}', 'Topics', $row);
-				if($robot) {
-					$row = str_replace('{topics_list_box.row.U_VIEW_TOPIC}', $key . '-topic.html', $row);
-				} else {
-					$row = str_replace('{topics_list_box.row.U_VIEW_TOPIC}', 'viewtopic.php?t=' . substr($key, 1), $row);
-				}
-				$row = str_replace('{topicrow.TOPIC_FOLDER_IMG_ALT}', $status, $row);
-				$row = str_replace('{topics_list_box.row.FOLDER_CLASSNAME}', $status, $row);
-				$row = str_replace('{topics_list_box.row.TOPIC_TYPE}', $topic_type, $row);
-				$row = str_replace('{topics_list_box.row.TOPIC_TITLE}', $value->{'name'}, $row);
-				$row = str_replace('{topics_list_box.row.REPLIES}', $value->{'replies'}, $row);
-				$row = str_replace('{L_REPLIES}', ' Replies', $row);
-				$row = str_replace('{L_IN}', '  ', $row);
-				$row = str_replace('{topics_list_box.row.VIEWS}', $value->{'views'}, $row);
-				$row = str_replace('{L_VIEWS}', ' Views', $row);
-				$row = str_replace('{topics_list_box.row.L_LATEST_POST_FROM_THE}', $value->{'lastpost'}, $row);
-
-
-				if ($key !== 't1') {
-					$row = preg_replace('/<\!\-\-\sBEGIN\sheader_table\s\-\->[\s\S]+<\!\-\-\sEND\sheader_table\s\-\->/', '<!-- Header table -->', $row);
-				}
-				if ($key !== 't7') {
-					$row = preg_replace('/<\!\-\-\sBEGIN\stable_sticky\s\-\->[\s\S]+<\!\-\-\sEND\stable_sticky\s\-\->/', '<!-- Table sticky -->', $row);
-				}
-				if ($key !== 't13') {
-					$row = preg_replace('/<\!\-\-\sBEGIN\sbottom\s\-\->[\s\S]+<\!\-\-\sEND\sbottom\s\-\->/', '<!-- Bottom -->', $row);
-				}
-
-				$txt .= $row;
-			}
-
-			return $txt;
-
-		}, $topics_list);
-
-	}
-
-	return $topics_list;
-
 }
 
 ?>
--- translate/viewforum.php
+++ translate/viewforum.php
@@ -44,7 +44,17 @@ if (preg_match('/^f(1|2|3|4|5|6|7|8|9|10|11)$/', $selected_id)) {
 // HEADER
 include 'inc/header.php';
 
+// TOPICS LIST
 if ($has_forum) {
+
+	$topics_list = file_get_contents('../templates/' . $path . '/topics_list_box.php');
+
+	function tempTopicsList($code, $html) {
+		global $topics_list;
+
+		return str_replace('{' . $code . '}', $html, $topics_list);
+	}
+
 	$viewforum = file_get_contents('../templates/' . $path . '/viewforum_body.php');
 	$dataObj = $categoriesObj;
 	$dataSub = 'forums';
@@ -68,7 +78,82 @@ if ($has_forum) {
 		$viewforum = str_replace('{BOARD_INDEX}', '', $viewforum);
 	}
 	if (preg_match('/^f\d+$/', $selected_id)) {
-		$viewforum = str_replace('{TOPICS_LIST_BOX}', topics_list(), $viewforum);
+
+		$topics_list = preg_replace('/<\!\-\-\sBEGIN\smulti_selection\s\-\->([\s\S]+?)<\!\-\-\sEND\smulti_selection\s\-\->/', '<!-- Multi selection -->', $topics_list);
+
+		$topics_list = preg_replace('/<\!\-\-\sBEGIN\sheader_row\s\-\->([\s\S]+)<\!\-\-\sEND\sheader_row\s\-\->/', '<!-- Header row -->', $topics_list);
+		$topics_list = str_replace('<!-- BEGIN spacer --><br /><!-- END spacer -->', '<!-- Spacer -->', $topics_list);
+
+		if (preg_match('/^f(6|11)$/', $selected_id)) {
+
+			$topics_list = preg_replace('/<\!\-\-\sBEGIN\stopic\s\-\->([\s\S]+)<\!\-\-\sEND\stopic\s\-\->/', '<!-- Topic -->', $topics_list);
+			$topics_list = tempTopicsList('topics_list_box.row.L_TITLE', 'Topics');
+			$topics_list = tempTopicsList('topics_list_box.row.L_NO_TOPICS', 'No message.');
+
+		} else {
+
+			$topics_list = preg_replace('/<\!\-\-\sBEGIN\sno_topics\s\-\->([\s\S]+)<\!\-\-\sEND\sno_topics\s\-\->/', '<!-- No topic -->', $topics_list);
+
+			$topics_list = preg_replace_callback('/<\!\-\-\sBEGIN\srow\s\-\->([\s\S]+)<\!\-\-\sEND\srow\s\-\->/', function ($arg) {
+				global $robot;
+				$topicsObj = json_decode(file_get_contents('../translate/data/topics.json'));
+
+				$topicrow = $arg[1];
+				$txt = '';
+
+				foreach ($topicsObj as $key => $value) {
+					$row = $topicrow;
+					$status = $value->{'status'};
+					$topic_type = '';
+					if (preg_match('/_global/', $status)) {
+						$topic_type = '<strong>Global announcement:</strong> ';
+					} else if (preg_match('/_announce/', $status)) {
+						$topic_type = '<strong>Announcement:</strong> ';
+					} else if (preg_match('/_sticky/', $status)) {
+						$topic_type = '<strong>Sticky:</strong> ';
+					}
+					if (!empty($value->{'poll'})) {
+						$topic_type .= '<strong>[ Poll ]</strong> ';
+					}
+
+					$row = str_replace('{topics_list_box.row.L_TITLE}', 'Announcement & Sticky', $row);
+					$row = str_replace('{topics_list_box.row.topic.table_sticky.L_TITLE}', 'Topics', $row);
+					if ($robot) {
+						$row = str_replace('{topics_list_box.row.U_VIEW_TOPIC}', $key . '-topic.html', $row);
+					} else {
+						$row = str_replace('{topics_list_box.row.U_VIEW_TOPIC}', 'viewtopic.php?t=' . substr($key, 1), $row);
+					}
+					$row = str_replace('{topicrow.TOPIC_FOLDER_IMG_ALT}', $status, $row);
+					$row = str_replace('{topics_list_box.row.FOLDER_CLASSNAME}', $status, $row);
+					$row = str_replace('{topics_list_box.row.TOPIC_TYPE}', $topic_type, $row);
+					$row = str_replace('{topics_list_box.row.TOPIC_TITLE}', $value->{'name'}, $row);
+					$row = str_replace('{topics_list_box.row.REPLIES}', $value->{'replies'}, $row);
+					$row = str_replace('{L_REPLIES}', ' Replies', $row);
+					$row = str_replace('{L_IN}', '  ', $row);
+					$row = str_replace('{topics_list_box.row.VIEWS}', $value->{'views'}, $row);
+					$row = str_replace('{L_VIEWS}', ' Views', $row);
+					$row = str_replace('{topics_list_box.row.L_LATEST_POST_FROM_THE}', $value->{'lastpost'}, $row);
+
+					if ($key !== 't1') {
+						$row = preg_replace('/<\!\-\-\sBEGIN\sheader_table\s\-\->[\s\S]+<\!\-\-\sEND\sheader_table\s\-\->/', '<!-- Header table -->', $row);
+					}
+					if ($key !== 't7') {
+						$row = preg_replace('/<\!\-\-\sBEGIN\stable_sticky\s\-\->[\s\S]+<\!\-\-\sEND\stable_sticky\s\-\->/', '<!-- Table sticky -->', $row);
+					}
+					if ($key !== 't13') {
+						$row = preg_replace('/<\!\-\-\sBEGIN\sbottom\s\-\->[\s\S]+<\!\-\-\sEND\sbottom\s\-\->/', '<!-- Bottom -->', $row);
+					}
+
+					$txt .= $row;
+				}
+
+				return $txt;
+
+			}, $topics_list);
+
+		}
+
+		$viewforum = str_replace('{TOPICS_LIST_BOX}', $topics_list, $viewforum);
 	} else {
 		$viewforum = str_replace('{TOPICS_LIST_BOX}', '', $viewforum);
 	}
